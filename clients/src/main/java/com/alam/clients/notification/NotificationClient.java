dfddd4b56addd656b99a73c3d32e3e841d0c9a8d
alamsn <28.alamnugroho@gmail.com>
2022-09-05 13:28:08 +0700

    Kubernetesable

--- clients/src/main/java/com/alam/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/alam/clients/notification/NotificationClient.java
@@ -4,7 +4,10 @@ import com.alam.clients.notification.entity.NotificationRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
 
     @PostMapping("api/v1/notification")
--- notification/src/main/java/com/alam/notification/NotificationApplication.java
+++ notification/src/main/java/com/alam/notification/NotificationApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -17,6 +19,9 @@ import org.springframework.context.annotation.Bean;
 )
 @EnableEurekaClient
 @EnableFeignClients
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")}
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
